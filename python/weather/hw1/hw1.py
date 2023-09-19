b59835d2a61215839da310d51c67d4cb0041bc5a
Allen Chen <chenyenlun@Allens-MacBook-Air-M2.local>
2023-09-19 19:07:03 +0800

    hw1 9/19

--- python/weather/hw1/hw1.py
+++ python/weather/hw1/hw1.py
@@ -2,16 +2,31 @@ import numpy as np
 import matplotlib.pyplot as plt
 from metpy.plots import SkewT
 from metpy.units import units
+import metpy.calc as mpcalc
 import pandas as pd 
 
 data = np.loadtxt('46757-2022062500.txt', delimiter=",", skiprows=2, unpack='true')
 
 h = data[1]
-p = data[2]
-t = data[3]
-td = data[5]
-ws = data[6]
-wd = data[7]
+p = data[2]*units.hPa
+t = data[3]*units.degC
+td = data[5]*units.degC
+ws = data[6]*1.9438*units.knots
+wd = data[7]*units.degrees
+prof = mpcalc.parcel_profile(p,t[0], td[0]).to('degC')
+a,b,c,profm = mpcalc.parcel_profile_with_lcl(p,t, td)
+
+lcl_p, lcl_t = mpcalc.lcl(p[0], t[0], td[0])
+print(lcl_p, lcl_t,"\n")
+
+lfc_p, lfc_t = mpcalc.lfc(p, t, td, prof)
+print(lfc_p, lfc_t,"\n")
+
+el_p, el_t = mpcalc.el(p, t, td, prof)
+print(el_p, el_t,"\n")
+
+cape, cin = mpcalc.cape_cin(p, t, td, prof)
+print(cape, cin)
 
 '''
 def wsd2uv(ws, wd):
@@ -22,21 +37,21 @@ def wsd2uv(ws, wd):
     return(x, y)
 '''
 
-u = -ws*np.sin(wd)
-v = -ws*np.cos(wd)
+u,v = mpcalc.wind_components(ws,wd)
 
 fig = plt.figure(figsize=(10,10))
 skew = SkewT(fig,rotation=45)              #叫出斜溫圖
 skew.plot(p,t,color='b')         #PT線
 skew.plot(p,td,color='r')         #PTd線
-skew.plot_barbs(p,u,v,xloc=1.1)     #風標，繪製於圖右側
+skew.plot_barbs(p,u,v,xloc=1.057)     #風標，繪製於圖右側
 skew.plot_dry_adiabats(t0=np.arange(-20,151,10)*units.celsius)    #繪製乾絕熱線
 skew.plot_moist_adiabats(t0=np.arange(-20,46,5)*units.celsius)    #繪製濕絕熱線
 skew.ax.set_title('2020082100')
-box = skew.ax.get_position()               #這四行繪製圖右側風標的竿子
-ax2 = fig.add_axes([box.x1*1.07,box.y0*0.999,0,box.y0+box.height*0.835])
-ax2.set_yticks()
-ax2.set_xticks()
+skew.plot(p,prof,'k')
+#skew.plot(p,profm,'k')
+skew.shade_cape(p,t,prof)
+skew.shade_cin(p,t,prof)
+
 
 
 
