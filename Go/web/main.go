8b35f8218f7da6be989cf81b469bd861ac900e24
Allen Chen <chenyenlun@Allens-MacBook-Air-M2.local>
2023-09-25 10:28:19 +0800

    go web development

---
+++ Go/web/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+    "fmt"
+    "html/template"
+    "net/http"
+    "os"
+	"io"
+)
+
+func uploadFile(w http.ResponseWriter, r *http.Request) {
+    if r.Method == "GET" {
+        // 顯示上傳文件的表單
+        t, _ := template.ParseFiles("/Users/chenyenlun/Desktop/Github/PC/Go/web/upload.gtpl")
+        t.Execute(w, nil)
+    } else if r.Method == "POST" {
+        // 處理上傳的文件
+        file, handler, err := r.FormFile("file")
+        if err != nil {
+            fmt.Println(err)
+            return
+        }
+        defer file.Close()
+
+        // 創建一個新文件來保存上傳的文件
+        storagePath := "/Users/chenyenlun/Desktop/Github/PC/Go/web/received_files/" + handler.Filename
+        dst, err := os.Create(storagePath)
+        if err != nil {
+            fmt.Println(err)
+            return
+        }
+        defer dst.Close()
+
+        // 將上傳的文件內容複製到新文件中
+        if _, err := io.Copy(dst, file); err != nil {
+            fmt.Println(err)
+            return
+        }
+
+        fmt.Fprintf(w, "上傳成功: %s", handler.Filename)
+    }
+}
+
+func main() {
+    http.HandleFunc("/upload", uploadFile)
+    http.ListenAndServe(":8080", nil)
+}
---
+++ Go/web/upload.gtpl
@@ -0,0 +1,15 @@
+<html>
+    <head>
+		<title>Mingey's Website</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+    <body>
+        <form action="/upload" method="post" enctype="multipart/form-data">
+            <input type="file" name="file" />
+            <input type="submit" value="上傳" />
+        </form>
+    </body>
+</html>
