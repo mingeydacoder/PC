e356038f844c2bda499b9c18cf093791963a7e8f
Allen Chen <chenyenlun@Allens-MacBook-Air-M2.local>
2023-10-11 15:58:17 +0800

    cloud physics hw1

---
+++ python/Cloud_Physics/HW1.c
@@ -0,0 +1,22 @@
+//Cloud Physics HW1 calculations
+#include <stdio.h> 
+#include <stdlib.h>
+#include <math.h>
+
+int main() 
+{ 
+    
+    float constant;
+    int T,P,cp,Rd,cw;
+    double Lv;
+    T = 305;
+    P = 1015;
+    cp = 1004;
+    Rd = 287;
+    cw = 187;
+    Lv = 2.5*pow(10,6);
+
+    printf("%0.2f",Lv);
+
+    return 0;
+}
