7b22aaa85b99b6027f479269b3a73221dc1c38a4
mingeydacoder <allenchen0121@gmail.com>
2024-04-04 20:51:04 +0800

    0404

--- Python/linebot_mingey.py
+++ Python/linebot_mingey.py
@@ -2,12 +2,13 @@
 #開啟另一終端輸入 ngrok http 5000
 #修改line bot後台 webhook URL
 
+import random
 import os
 from openai import OpenAI
 
 client = OpenAI(
     # This is the default and can be omitted
-    api_key="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"
+    api_key="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"
 )
 
 from flask import Flask, request
@@ -33,6 +34,7 @@ def linebot():
         msg = json_data['events'][0]['message']['text']
         # 取出文字的前五個字元，轉換成小寫
         ai_msg = msg[:6].lower()
+        draw_msg = msg[:1]
         reply_msg = ''
         # 取出文字的前五個字元是 hi ai:
         if ai_msg == 'hi ai:':
@@ -49,6 +51,21 @@ def linebot():
             # 接收到回覆訊息後，移除換行符號
             reply_msg = chat_completion.choices[0].message.content
             #reply_msg = "worked"
+        
+        if draw_msg == '!' or draw_msg == '！':
+            # 定義兩個選項
+            option1 = "YES"
+            option2 = "NO"
+
+            # 隨機生成一個 0 或 1 的數字
+            choice = random.randint(0, 1)
+
+            # 根據隨機數字選擇選項
+            if choice == 0:
+                reply_msg = option1
+            else:
+                reply_msg = option2
+
         text_message = TextSendMessage(text=reply_msg)
         line_bot_api.reply_message(tk,text_message)
     except:
